7833be7520f632ca1c718fe37458afa6ca0a537c
DevTorchez <torchez@yandex.ru>
2023-11-24 18:51:21 +0300

    Resolve Chapter1.Task2

--- src/main/java/com/walking/intensive/chapter1/task2/Task2.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Task2.java
@@ -4,14 +4,57 @@ package com.walking.intensive.chapter1.task2;
  * Условие: <a href="https://geometry-math.ru/homework/Java-house.html">ссылка</a>
  */
 public class Task2 {
-    public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+    static String leftOrRight = "";
+    static int setEntrance, setFloor;
 
+    public static void main(String[] args) {
+
+        //Параметры тестов
+        System.out.println(getFlatLocation(10, 3, 40));
+        System.out.println(getFlatLocation(10, 3, 41));
+        System.out.println(getFlatLocation(10, 3, 88));
+        System.out.println(getFlatLocation(2, 2, 5));
+        System.out.println(getFlatLocation(2, 2, 9));
+        System.out.println(getFlatLocation(10, 3, -1));
+        System.out.println(getFlatLocation( 2, 2, -1));
+        System.out.println(getFlatLocation( 10, 3, 121));
+        System.out.println(getFlatLocation(2, 2, 17));
+
+    }
+    static int getFloor(int floorAmount,int flatNumber){
+        setFloor = floorAmount;
+        int maxFlat = getEntrance(floorAmount, flatNumber) * floorAmount * 4;
+        while (maxFlat - 3 > flatNumber){
+            setFloor = setFloor - 1;
+            maxFlat = maxFlat - 4;
+        }
+        return setFloor;
+    }
+    static int getEntrance(int floorAmount,int flatNumber) {
+        setEntrance = 1;
+        while ((setEntrance * floorAmount * 4) < flatNumber) {
+            setEntrance++;
+        }
+        return setEntrance;
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        //        Место для вашего кода
+        setFloor = floorAmount;
 
-        return null; // Заглушка. При реализации - удалить
+        int totalFlatQty = floorAmount * 4 * entranceAmount;
+
+        if (flatNumber > totalFlatQty || flatNumber <= 0) {
+            return "Такой квартиры не существует";
+        }
+
+        switch (flatNumber % 4) {
+            case 1, 2 -> leftOrRight = ", слева от лифта, влево";
+            case 3, 0 -> leftOrRight = ", справа от лифта, вправо";
+        }
+
+        String totalString = flatNumber + " кв - " + getEntrance(floorAmount,flatNumber) + " подъезд, "
+                + getFloor(floorAmount, flatNumber) + " этаж"  + leftOrRight;
+
+        return totalString;
     }
 }
